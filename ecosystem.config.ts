4ffad0824bda6a9640654849393fc05d8d25aff0
Edward <eduard.tymoshuk@gmail.com>
2025-08-13 20:39:39 +0200

    fix: fix deploy issues

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -12,6 +12,8 @@ module.exports = {
         NODE_ENV: 'production',
         NEXTAUTH_URL: 'http://185.242.135.184:3000',
         AUTH_TRUST_HOST: 'true',
+        DATABASE_URL:
+          'postgresql://huzzar_admin:Huzz%40r2025%21@185.242.135.184:5432/huzzar_vectra_crm',
       },
     },
   ],
