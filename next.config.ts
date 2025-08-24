42177e359602179077c42910af3a85e98369ad1a
Edward <eduard.tymoshuk@gmail.com>
2025-08-24 23:23:36 +0200

    fix: fix file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
