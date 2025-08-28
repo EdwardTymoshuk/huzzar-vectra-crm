a77ea99cd5e919a886886a9411ed8e74a075e9bf
Edward <eduard.tymoshuk@gmail.com>
2025-08-28 22:27:47 +0200

    fix: fix issues

--- src/server/routers/context.ts
+++ src/server/routers/context.ts
@@ -1,10 +1,11 @@
-import { authOptions } from '@/lib/authOptions'
+import { getAuthOptions } from '@/lib/authOptions'
 import { CreateNextContextOptions } from '@trpc/server/adapters/next'
 import { getServerSession } from 'next-auth'
 
 export async function createContext(opts: CreateNextContextOptions) {
   const { req, res } = opts
-  const session = await getServerSession(req, res, authOptions)
+  const authOptions = await getAuthOptions()
+  const session = await getServerSession(authOptions)
 
   return {
     user: session?.user ?? null,
