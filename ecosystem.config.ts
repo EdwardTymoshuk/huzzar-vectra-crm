8943ecb50df9a9636e1be89f93ccc5db555da894
Edward <eduard.tymoshuk@gmail.com>
2025-08-29 08:11:06 +0200

    fix: fix errors

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -10,7 +10,7 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: 'production',
-        NEXTAUTH_URL: 'http://http://57.128.227.195/',
+        NEXTAUTH_URL: 'http://57.128.227.195/',
         AUTH_TRUST_HOST: 'true',
         DATABASE_URL:
           'postgresql://huzzar_admin:Huzz%40r2025%21@57.128.227.195/huzzar_vectra_crm',
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,11 @@
 // src/app/api/auth/[...nextauth]/route.ts
-export const runtime = 'nodejs'
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 import { getAuthOptions } from '@/lib/authOptions'
 import NextAuth from 'next-auth'
 
 const handler = async (req: Request) => {
   const authOptions = await getAuthOptions()
-  const nextAuthHandler = NextAuth(authOptions)
-  return nextAuthHandler(req)
+  return NextAuth(authOptions)(req)
 }
 
-export { handler as GET, handler as POST }
+export const GET = handler
+export const POST = handler
