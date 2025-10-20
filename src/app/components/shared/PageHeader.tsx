3761e01a23db8696374c809f20decdbca57e6af4
Edward <eduard.tymoshuk@gmail.com>
2025-10-20 19:59:46 +0200

    fix: fix header centering

--- src/app/components/shared/PageHeader.tsx
+++ src/app/components/shared/PageHeader.tsx
@@ -1,6 +1,6 @@
 const PageHeader = ({ title }: { title: string }) => {
   return (
-    <div className="p-2 w-full flex items-center justify-start text-primary">
+    <div className="p-2 w-full flex items-center justify-center md:justify-start text-primary">
       <h1 className="font-semibold text-2xl text-center h-fit">{title}</h1>
     </div>
   )
